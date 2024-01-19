1f34f4a0bc9d828bbe8d820841663c0b51ced134
dp2023 <74738050+dp2023@users.noreply.github.com>
2024-01-19 13:30:40 -0500

    Initial commit

---
+++ clipSeg.py
@@ -0,0 +1,26 @@
+from transformers import CLIPSegProcessor, CLIPSegForImageSegmentation
+from PIL import Image
+import requests
+import torch
+import matplotlib.pyplot as plt
+
+
+
+processor = CLIPSegProcessor.from_pretrained("CIDAS/clipseg-rd64-refined")
+model = CLIPSegForImageSegmentation.from_pretrained("CIDAS/clipseg-rd64-refined")
+image = Image.open("/mnt/c/Users/diego/Documents/GRASP-Lab/Human-Segmentation-3DImages/Images/frame_00429.png")
+
+print("Loaded Image")
+prompts = ["people", "cars"]
+
+inputs = processor(text=prompts, images=[image] * len(prompts), padding="max_length", return_tensors="pt")
+# predict
+print("Showing Results")
+with torch.no_grad():
+  outputs = model(**inputs)
+preds = outputs.logits.unsqueeze(1)
+_, ax = plt.subplots(1, len(prompts) + 1, figsize=(3*(len(prompts) + 1), 4))
+[a.axis('off') for a in ax.flatten()]
+ax[0].imshow(image)
+[ax[i+1].imshow(torch.sigmoid(preds[i][0])) for i in range(len(prompts))]
+[ax[i+1].text(0, -15, prompt) for i, prompt in enumerate(prompts)]
